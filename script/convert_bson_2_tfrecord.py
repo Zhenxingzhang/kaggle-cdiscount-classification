28dab3dc381aa2c71b3c5845578aaa7a149e58b1
zzhang <zhenxing.zhang@zalando.ie>
2017-10-24 10:34:01 +0100

    update the script

--- script/convert_bson_2_tfrecord.py
+++ script/convert_bson_2_tfrecord.py
@@ -21,14 +21,14 @@ def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
-def convert_bson_2_record(input_bson_filename, output_tfrecords_filename, train=False):
+def convert_bson_2_record(input_bson_filename, output_tfrecords_filename, n=10000):
 
-    # on my workstation it takes about 5 min per 100k entries, so should finish in about 6h
+    z = 0
     data = bson.decode_file_iter(open(input_bson_filename, 'rb'))
     opts = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.ZLIB)
 
     with tf.python_io.TFRecordWriter(output_tfrecords_filename,  options=opts) as writer:
-        for c, d in tqdm(enumerate(data), total=1768182):
+        for c, d in tqdm(enumerate(data), total=n):
             n_img = len(d['imgs'])
             for index in range(n_img):
                 img_raw = d['imgs'][index]['picture']
@@ -47,11 +47,17 @@ def convert_bson_2_record(input_bson_filename, output_tfrecords_filename, train=
                 example = tf.train.Example(features=tf.train.Features(feature=_feature))
                 writer.write(example.SerializeToString())
 
+            z = z + 1
+            if z % n == 0:
+                print(z)
+                break
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-i', dest="bson_filename", type=str, required=True, help='')
-    parser.add_argument('-o', dest="tfrecord_filename", type=str, required=True, help='')
+    parser.add_argument('-i', dest="bson_filename", type=str, required=True, help='the input file in bson format')
+    parser.add_argument('-o', dest="tfrecord_filename", type=str, required=True, help='the output file in tfrecrods format')
+    parser.add_argument('-n', dest="total_records", type=int, required=True, help='number of records to convert.')
     args = parser.parse_args()
 
-    convert_bson_2_record(args.bson_filename, args.tfrecord_filename)
+    convert_bson_2_record(args.bson_filename, args.tfrecord_filename, args.total_records)
