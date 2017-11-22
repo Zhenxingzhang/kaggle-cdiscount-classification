4b9dbf014e0af411419dcb4ea7238b78dbfe39dc
zzhang <zhenxing.zhang@zalando.ie>
2017-11-22 11:51:25 +0000

    random sample bson working

--- src/data_preparation/random_sample_bson_file.py
+++ src/data_preparation/random_sample_bson_file.py
@@ -1,32 +1,30 @@
-import bson
-import random
+from bson import BSON
+import random, bson
 from tqdm import tqdm
 import argparse
 
 
-def convert_bson_2_record(input_bson_filename, output_bson_filename,  n=100, number_random_example=10):
+def random_sample_bson(input_bson_filename, output_bson_filename,  n=100, number_random_example=10):
 
     data = bson.decode_file_iter(open(input_bson_filename, 'rb'))
 
-    print(n, number_random_example)
     random_items = random.sample(range(n), number_random_example)
     random_items.sort()
 
-    idx = 0
     r_idx = 0
 
+    output = open(output_bson_filename, 'w+')
+
     for c, d in tqdm(enumerate(data), total=n):
-        print(idx, random_items[r_idx])
-        if idx != random_items[r_idx]:
-            idx = idx + 1
+        if c != random_items[r_idx]:
             continue
         else:
-            # for c, d in enumerate(data):
-            idx = idx + 1
+            # print("pick random item: {}".format(c))
+            # insert your code here.
+            output.write(BSON.encode(d))
             r_idx = r_idx + 1
-
             if r_idx >= number_random_example:
-                    break
+                break
 
     print("Finish convert tfrecords with {} records".format(r_idx))
 
@@ -39,5 +37,5 @@ if __name__ == "__main__":
     parser.add_argument('-r', dest="number_of_random_records", type=int, required=True, help='number of random records to convert.')
     args = parser.parse_args()
 
-    convert_bson_2_record(args.full_bson_filename, args.subset_bson_filename,
-                          n=args.total_records, number_random_example=args.number_of_random_records)
+    random_sample_bson(args.full_bson_filename, args.subset_bson_filename,
+                       n=args.total_records, number_random_example=args.number_of_random_records)
