36862104c0dafd7643e547b1ee9367b75b62c096
zzhang <zhenxing.zhang@zalando.ie>
2017-12-01 12:26:06 +0000

    update tensorboard dir

--- run_tools.sh
+++ run_tools.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir /data/tensorboard-logs/
-
-tensorboard --logdir /data/tensorboard-logs/ &
+tensorboard --logdir /data/summary/ &
 
 jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --notebook-dir='/notebooks' "$@"
